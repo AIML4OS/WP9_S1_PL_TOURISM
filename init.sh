30b7b1538e15ca3417410496b1e22e7e0e26d330
Jarek Napora <126453347+JarekNapora@users.noreply.github.com>
2025-11-13 12:47:35 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,7 +8,8 @@ export MY_REPO=$(ls -d "/home/onyxia/work"/*/ | head -n 1 | xargs basename)
 echo /home/onyxia/work/$MY_REPO
 
 # Download data
-sh $MY_REPO/sspcloud/download_data.sh
+# This script is run with bash because there is a bash array
+bash $MY_REPO/sspcloud/download_data.sh
 
 # Open project
 sh $MY_REPO/sspcloud/open_project.sh
